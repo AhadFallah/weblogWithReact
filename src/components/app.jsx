7d5512bd9d142c9052ec79c8c2f9a8366b1ed919
AhadFallah <ahad.fallah.k@gmail.com>
2024-06-10 00:35:51 +0330

    initial navbar

---
+++ src/components/app.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Navbar from "./partials/navbar.jsx";
+class App extends Component {
+  render() {
+    return <Navbar />;
+  }
+}
+export default App;
