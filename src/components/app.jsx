957acb3f9bef8ba9ba8bc24d34802abfb40c3632
AhadFallah <ahad.fallah.k@gmail.com>
2024-06-11 23:03:04 +0330

    make persian menu

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Navbar from "./partials/navbar.jsx";
 class App extends Component {
-  render() {
-    return <Navbar />;
-  }
+	render() {
+		return <Navbar />;
+	}
 }
 export default App;
