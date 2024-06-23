c2ca69594597c273422a6d6d62d131ef67b52773
AhadFallah <ahad.fallah.k@gmail.com>
2024-06-23 16:51:09 +0330

    fix cards for better looking

--- src/components/contents/card.jsx
+++ src/components/contents/card.jsx
@@ -7,25 +7,22 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 class Card extends Component {
-	render() {
-		const cardStyle = {
-			width: "18rem",
-		};
-		const rtlStyle = {
-			"text-align": "right",
-		};
-		return (
-			<div className="card m-4" style={cardStyle} dir="rtl">
-				<img src={this.props.img} alt="تصویر" className="card-img-top" />
-				<div className="card-body" style={rtlStyle}>
-					<h1 className="card-title">{this.props.title}</h1>
-					<p className="card-text">{this.props.description}</p>
-					<a href="" className="btn btn-primary">
-						ادامه مطلب
-					</a>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const rtlStyle = {
+      "text-align": "right",
+    };
+    return (
+      <div className="card m-4" dir="rtl">
+        <img src={this.props.img} alt="تصویر" className="card-img-top" />
+        <div className="card-body" style={rtlStyle}>
+          <h1 className="card-title">{this.props.title}</h1>
+          <p className="card-text">{this.props.description}</p>
+          <a href="" className="btn btn-primary">
+            ادامه مطلب
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 export default Card;
--- src/components/contents/home.jsx
+++ src/components/contents/home.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Navbar from "../partials/navbar.jsx";
 import Card from "../contents/card.jsx";
 
+import "./home.css";
+
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -9,21 +11,41 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 
 class Home extends Component {
-	render() {
-		return (
-			<div>
-				<div>
-					<Navbar />
-				</div>
-				<div dir="rtl">
-					<Card
-						title="article"
-						description="alie"
-						img="https://files.virgool.io/upload/users/3182518/posts/mdtyspslbcan/0stogdlhqipb.webp"
-					/>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div dir="rtl">
+        <div>
+          <Navbar />
+        </div>
+        <div className="card-columns m-5">
+          <Card
+            title="article"
+            description="alie"
+            img="https://files.virgool.io/upload/users/3182518/posts/mdtyspslbcan/0stogdlhqipb.webp"
+          />{" "}
+          <Card
+            title="article"
+            description="alie"
+            img="https://files.virgool.io/upload/users/3182518/posts/mdtyspslbcan/0stogdlhqipb.webp"
+          />{" "}
+          <Card
+            title="article"
+            description="alie"
+            img="https://files.virgool.io/upload/users/3182518/posts/mdtyspslbcan/0stogdlhqipb.webp"
+          />{" "}
+          <Card
+            title="article"
+            description="alie"
+            img="https://files.virgool.io/upload/users/3182518/posts/mdtyspslbcan/0stogdlhqipb.webp"
+          />{" "}
+          <Card
+            title="article"
+            description="alie"
+            img="https://files.virgool.io/upload/users/3182518/posts/mdtyspslbcan/0stogdlhqipb.webp"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 export default Home;
